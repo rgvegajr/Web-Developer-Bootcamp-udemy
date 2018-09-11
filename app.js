76d0a4e686b9660995803b2cc54e2d57f7a82fe7
rgvegajr <rgvegajr@gmail.com>
2018-09-11 00:12:56 +0000

    changed database to mlab

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+//mongoose.connect("mongodb://localhost/yelp_camp_v12");
+//mongodb://RV:simba1@ds251622.mlab.com:51622/yelpcamp
+mongoose.connect("mongodb://rv:simba1@ds251622.mlab.com:51622/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
